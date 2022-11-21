ae95640b716437149801ff3f64cc4321c4626f84
-Andreas <andreaschristensen2010@live.dk>
2022-11-21 14:56:42 +0100

    readme and code

---
+++ MapDataLibrary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+
+namespace Example
+{
+    public class MapDataLibrary
+    {
+        /* The defined map borders used when getting data from files and from the data file */
+        private static Tuple<float, float> mapX = new Tuple<float, float>(-4100f, 4300f);
+        private static Tuple<float, float> mapY = new Tuple<float, float>(-4300f, 7825f);
+
+        /* Data file */
+        private const string HEIGHTDATA_FILE = @"data_files/GTAV_HeightMap_Data.data";
+
+        public static float GetHeightAtXY(float posX, float posY)
+        {
+            if (!ContainPosition(posX, posY))
+                return 0f;
+
+            MemoryMappedFile mmf;
+            try
+            {
+                mmf = MemoryMappedFile.CreateFromFile(HEIGHTDATA_FILE, FileMode.Open);
+            }
+            catch (DirectoryNotFoundException dirEx)
+            {
+                Console.WriteLine("Directory not found: " + dirEx.Message + " proceeding with 0.0f");
+                // Make this functional even if the file does not exit.
+                return 0.0f;
+            }
+
+            using (mmf)
+            {
+                var x = (int)posX - (int)mapX.Item1;
+                var y = (long)(mapX.Item2 - mapX.Item1) * ((long)posY - (long)mapY.Item1);
+
+                using (var accessor = mmf.CreateViewAccessor((y + x) * 4, 4))
+                {
+                    return accessor.ReadSingle(0);
+                }
+            }
+        }
+
+        private static bool ContainPosition(float x, float y)
+        {
+            return mapX.Item1 <= x && x < 4100 && mapY.Item1 <= y && y <= mapY.Item2;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ragemp-gtav-heightmap
  
+The repository contains a helpful file with Z-heights for any given X,Y coordinate in GTA:V. I originally wrote a client-side script in JavaScript back in 2018 that would teleport the player in a grid pattern and grab the heights around him using
+```
+entity.getHeight(X, Y, Z, atTop, inWorldCoords);
+```
+from https://wiki.rage.mp/index.php?title=Entity::getHeight
+This is not perfect, and I'm pretty sure that there exists proper height maps that would produce the same.
+
+The result of running the height grabber was the file ```data_file/GTAV_HeightMap_Data.data```.
+All heights are within a bounded box with a lower left point and upper right point: 
+* LOWER LEFT: (-4100,-4300)
+* UPPER RIGHT: (4300, 7825)
+
+Beaware that some heights might be 0, but my experience is that this is only a few of them.
+
+<h2>To use</h2>
+
+Just invoke GetHeightAtXY(...) from MapDataLibrary as such:
+```
+float height = GetHeightAtXY(goto.X, goto.Y);
+NAPI.Entity.SetEntityPosition(ply, new Vector3(goto.X, goto.Y, height));
+```
